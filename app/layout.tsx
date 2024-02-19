4e7179035b1e9632ede8bb87c2d4c7ef0791ee4e
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-02-19 15:46:49 +0100

    init project

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next"
+import { DM_Sans } from "next/font/google"
+import "./globals.css"
+import Attribution from "./components/Attribution"
+
+const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "700"] })
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor Challenge",
+  description: "A Challenge from Frontend Mentor!",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
+      </head>
+      <body
+        className={`${dmSans.className} flex min-h-screen flex-col items-center`}
+      >
+        {children}
+        <Attribution />
+      </body>
+    </html>
+  )
+}
